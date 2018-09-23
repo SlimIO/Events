ea03728deae236c5b390fe5e1a6100109bf657f6
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2018-09-23 16:58:14 +0200

    Add SlimIO.is & remove registerEvent (types should be fixed nor dynamic)

--- src/events.addon.js
+++ src/events.addon.js
@@ -1,14 +1,16 @@
 // Require Node.JS Dependencies
-const { mkdir, readdir, readFile } = require("fs").promises;
+const { readdir, readFile } = require("fs").promises;
 const { join, extname, basename } = require("path");
 
 // Require Third-party Dependencies
 const levelup = require("levelup");
 const leveldown = require("leveldown");
 const protobuf = require("protocol-buffers");
+const is = require("@slimio/is");
 
 // Require Internal Dependencies
 const Addon = require("@slimio/addon");
+const { createDir } = require("./utils");
 
 // Create Addon!
 const Events = new Addon("events");
@@ -48,71 +50,25 @@ function closeDB(db) {
     if (db.isOpen()) {
         db.close();
     }
+    // eslint-disable-next-line
     db = null;
-}
-
-/**
- * @async
- * @function createDir
- * @param {String} path dbPath
- * @returns {Promise<void>}
- */
-async function createDir(path) {
-    try {
-        await mkdir(path);
-    }
-    catch (error) {
-        if (error.code !== "EEXIST") {
-            throw error;
-        }
-    }
-}
-
-/**
- * @typedef {Object} registerEventOptions
- * @property {Boolean=} dump
- * @property {Boolean=} storeLocally
- */
-
-/**
- * @async
- * @func registerEventType
- * @desc Register a new event type
- * @param {!String} name event name
- * @param {registerEventOptions=} options event options
- * @return {Promise<String>}
- */
-async function registerEventType(name, options = {}) {
-    if (name === "events") {
-        return "Event type name 'events' is INVALID!";
-    }
-    if (AVAILABLE_TYPES.has(name)) {
-        return `Event type with name ${name} is already registered!`;
-    }
-    AVAILABLE_TYPES.add(name);
-    const { storeLocally = true } = options;
-
-    if (storeLocally === true) {
-        const types = [...AVAILABLE_TYPES].join(",");
-        await db.put("types", types);
-    }
-
-    return null;
 }
 
 /**
  * @async
  * @func publishEvent
  * @desc Publish a new event!
- * @param {!String} type event type
- * @param {!Buffer} rawBuf buffer
+ * @param {!String} type event type name (name/destination)
+ * @param {!Buffer} rawBuf raw payload buffer
  * @return {Promise<Number>}
+ *
+ * @throws {Error}
  */
 async function publishEvent([type, rawBuf]) {
-    if (typeof type !== "string") {
+    if (!is.string(type)) {
         throw new TypeError("type should be typeof string");
     }
-    if (!Buffer.isBuffer(rawBuf)) {
+    if (!is.buffer(rawBuf)) {
         throw new TypeError("rawBuf should be typeof Buffer!");
     }
 
@@ -142,57 +98,20 @@ Events.on("start", async() => {
     db = openDB("events");
 
     // Load available Prototypes
-    {
-        const files = await readdir(PROTOTYPE_DIR_PATH);
-        for (const file of files) {
-            if (extname(file) !== ".proto") {
-                continue;
-            }
-            try {
-                const proto = protobuf(await readFile(join(PROTOTYPE_DIR_PATH, file)));
-                PROTOTYPES_TYPES.set(basename(file, ".proto"), proto);
-            }
-            catch (error) {
-                console.log(`[EVENTS] Failed to load prototype ${file} - ${error.toString()}`);
-            }
+    // TODO: Improve file loading with a Promise.all
+    const files = (await readdir(PROTOTYPE_DIR_PATH)).filter((fileName) => extname(fileName) === ".proto");
+    for (const file of files) {
+        try {
+            const proto = protobuf(await readFile(join(PROTOTYPE_DIR_PATH, file)));
+            PROTOTYPES_TYPES.set(basename(file, ".proto"), proto);
+        }
+        catch (error) {
+            console.log(`[EVENTS] Failed to load prototype ${file} - ${error.toString()}`);
         }
     }
 
     // Create all default types directory!
     await Promise.all(DEFAULT_EVENTS_TYPES.map((type) => createDir(join(DB_DIR_PATH, type))));
-
-    try {
-        /** @type {String} */
-        const types = (await db.get("types")).toString();
-        for (const type of types.split(",")) {
-            AVAILABLE_TYPES.add(type);
-        }
-    }
-    catch (err) {
-        // Do nothing...
-    }
-
-    // Return if every default types are loaded!
-    if (DEFAULT_EVENTS_TYPES.every((eT) => AVAILABLE_TYPES.has(eT) === true)) {
-        console.log("[EVENTS] All types already loaded!");
-
-        return;
-    }
-
-    /** @type {registerEventOptions} */
-    const defaultRegisterOption = { storeLocally: false };
-
-    // Setup all default types!
-    await Promise.all([
-        registerEventType("alarm", defaultRegisterOption),
-        registerEventType("metric", defaultRegisterOption),
-        registerEventType("log", defaultRegisterOption),
-        registerEventType("error", defaultRegisterOption)
-    ]);
-
-    //
-    await db.put("types", [...AVAILABLE_TYPES].join(","));
-    console.log("[EVENTS] All types updated successfully!");
 });
 
 // Event "stop" handler
@@ -201,7 +120,6 @@ Events.on("stop", () => {
 });
 
 // Register addon callback(s)
-Events.registerCallback("register_event_type", registerEventType);
 Events.registerCallback("publish_event", publishEvent);
 
 // Export addon
