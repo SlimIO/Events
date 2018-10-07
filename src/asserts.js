eeef424153df641811a81b134e13a5376f982e5b
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2018-10-07 20:50:49 +0200

    Work on entity declaration

---
+++ src/asserts.js
@@ -0,0 +1,24 @@
+// Require SlimIO.is
+const is = require("@slimio/is");
+
+function assertEntity(entity) {
+    if (!is.plainObject(entity)) {
+        throw new TypeError("entity should be a plainObject!");
+    }
+    if (!is.string(entity.name)) {
+        throw new TypeError("entity.name property should be typeof string");
+    }
+    if (!is.nullOrUndefined(entity.description) && !is.string(entity.description)) {
+        throw new TypeError("entity.description property should be typeof string");
+    }
+    if (!is.nullOrUndefined(entity.parent) && !is.string(entity.parent)) {
+        throw new TypeError("entity.parent property should be typeof string");
+    }
+    if (!is.nullOrUndefined(entity.descriptors) && !is.plainObject(entity.descriptors)) {
+        throw new TypeError("entity.descriptors should be a plainObject!");
+    }
+}
+
+module.exports = {
+    assertEntity
+};
--- src/events.addon.js
+++ src/events.addon.js
@@ -1,13 +1,16 @@
 // Require NodeJS Dependencies
 const { readFile } = require("fs").promises;
 const { join } = require("path");
-const { hostname } = require("os");
 
 // Require Third-Party Dependencies
 const Addon = require("@slimio/addon");
+const is = require("@slimio/is");
 const sqlite = require("better-sqlite3");
 const { createDirectory } = require("@slimio/utils");
-const uuidv5 = require("uuid/v5");
+const uuidv4 = require("uuid/v4");
+
+// Require Internal Dependencies
+const { assertEntity } = require("./asserts");
 
 // CONSTANTS
 const ROOT = join(__dirname, "..");
@@ -17,20 +20,51 @@ let db = null;
 // Create EVENTS Addon!
 const Events = new Addon("events");
 
-async function declareIdentity(entity) {
-    if (db === null || !db.open) {
-        throw new Error("Events DB not open!");
+async function declareEntityDescriptor(entityId, key, value) {
+    if (!Events.isReady) {
+        throw new Error("Events Addon is not yet ready!");
     }
 
+    db.prepare("INSERT INTO entity_descriptor (entity_id, key, value) VALUES (@id, @key, @value)")
+        .run({ id: entityId, key, value });
+}
+
+async function declareEntity(entity) {
+    if (!Events.isReady) {
+        throw new Error("Events Addon is not yet ready!");
+    }
+    assertEntity(entity);
+    const { name, parent, description, descriptors = {} } = entity;
+
+    // If the entity exist, then return the id
+    const row = db.prepare("SELECT id FROM entity WHERE name=@name").get({ name });
+    if (typeof row !== "undefined") {
+        for (const [key, value] of Object.entries(descriptors)) {
+            declareEntityDescriptor(row.id, key, value).catch(console.error);
+        }
+
+        return row.id;
+    }
+
+    // Else, create a new row for the entity!
     const stmt = db.prepare(
-        "INSERT INTO entity(uuid, name, parent, description) VALUES(uuid(@name), @name, @parent, @description)"
+        "INSERT INTO entity(uuid, name, parent, description) VALUES(uuid(), @name, @parent, @description)"
     );
-    stmt.run({
-        name: entity.name,
-        parent: entity.parent,
-        description: entity.description
-    });
-    // Do things..
+
+    const RowID = stmt.run({ name, parent, description }).lastInsertROWID;
+    for (const [key, value] of Object.entries(descriptors)) {
+        declareEntityDescriptor(row.id, key, value).catch(console.error);
+    }
+
+    return RowID;
+}
+
+async function removeEntity(entityId) {
+
+}
+
+async function getEntityOID(entityName) {
+
 }
 
 async function declareMetricIdentity() {
@@ -51,15 +85,18 @@ Events.on("start", async() => {
     await createDirectory(DB_DIR);
     db = new sqlite(join(DB_DIR, "events.db"));
     db.exec(await readFile(join(ROOT, "sql", "events.sql"), "utf8"));
-    db.register(function uuid(name) {
-        return uuidv5(hostname(), name);
+    db.register(function uuid() {
+        return uuidv4();
     });
 
-    Events.emit("ready");
+    Events.ready();
 });
 
 // Register addon callback(s)
-Events.registerCallback("declare_identity", declareIdentity);
+Events.registerCallback("declare_entity", declareEntity);
+Events.registerCallback("declare_entity_descriptor", declareEntityDescriptor);
+Events.registerCallback("remove_entity", removeEntity);
+Events.registerCallback("get_entity_oid", getEntityOID);
 Events.registerCallback("declare_mic", declareMetricIdentity);
 Events.registerCallback("publish_alarm", publishAlarm);
 Events.registerCallback("publish_metric", publishMetric);
