f845da2401a82b74fed7b3e5f3f2440792e7a87f
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2019-06-12 16:14:14 +0200

    refactor: evolve metric schema for aggregator

--- sql/metric.sql
+++ sql/metric.sql
@@ -1,4 +1,6 @@
 CREATE TABLE IF NOT EXISTS "metrics" (
     "value" INTEGER NOT NULL,
-    "harvestedAt" DATETIME NOT NULL
+    "type" CHAR(3) DEFAULT "raw" NOT NULL,
+    "harvestedAt" DATETIME NOT NULL,
+    "level" TINYINT DEFAULT 0 NOT NULL
 );
